7e54672e2e065cdaea76828a53ec448118ddb57a
chsaikumar445 <ch.saikumar445@gmail.com>
2024-01-11 09:29:16 -0600

    added img

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,21 @@
+import Header from "../components/Header";
+
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className="">
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d8a0fd20-b34b-405e-860f-53499c2517b7/US-en-20240108-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt="img1"
+        />
+      </div>
+      <form className="absolute flex">
+        <label>Sign In</label>
+        <input className="p-4" placeholder="Email" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
