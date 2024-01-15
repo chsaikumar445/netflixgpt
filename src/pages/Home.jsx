deff544aa813758800d683b7fe944ec6d0be7a26
chsaikumar445 <ch.saikumar445@gmail.com>
2024-01-14 18:37:51 -0600

    updated ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
         <div className="absolute">
           <img
             src="https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/d8a0fd20-b34b-405e-860f-53499c2517b7/US-en-20240108-popsignuptwoweeks-perspective_alpha_website_small.jpg"
-            alt="bgimg"
+            alt="bgimgnetflix"
           />
         </div>
         <div className="relative z-11 text-white top-60 left-40 w-1/2 m-auto">
