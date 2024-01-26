aae3f1e178748d83aaec57670d67765f9bc52902
chsaikumar445 <ch.saikumar445@gmail.com>
2024-01-25 21:36:30 -0600

    added sign out in browse page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="absolute z-10 top-5 left-20  ">
+    <div className="absolute bg-gradient-to-b from-black w-screen z-10 ">
       <Link to={"/"}>
         <img
-          className="w-48"
+          className="w-48 ml-20"
           src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
           alt="img1"
         />
--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import { auth } from "../utils/firebase";
+import { signOut } from "firebase/auth";
 
 const Browse = () => {
-  return <div>Browse</div>;
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch((error) => {
+        // An error happened.
+        navigate("/error");
+      });
+  };
+
+  return (
+    <div className="relative">
+      <Header />
+      <div className="absolute right-20 top-6 flex justify-between ">
+        <img
+          className=" w-10 z-10"
+          src="https://occ-0-92-90.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+          alt="icon"
+        />
+        <button
+          onClick={handleSignOut}
+          className="bg-red-600 pl-2 pr-2 ml-2 text-white z-10 rounded-md">
+          Sign Out
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Browse;
